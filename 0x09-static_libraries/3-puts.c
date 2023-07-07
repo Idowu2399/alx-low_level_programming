2d2bba3dc7710f2a5216e0b3f6b8df852f7a18cd
idowu2399 <idowuolamilekan039@gmail.com>
2023-07-07 10:14:29 -0700

    Update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints a string to stdout
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+puts(str);
+}
+
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+_puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
